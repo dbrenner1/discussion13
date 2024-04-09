80f1be778ffb9c4ac605ccb3e228bf54e327d75c
dbrenner1 <156814705+dbrenner1@users.noreply.github.com>
2024-04-09 17:50:53 -0400

    Add files via upload

---
+++ js/challenge3.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function(){
+    // alert('loaded!!');
+    let figcaptions = document.querySelectorAll('figcaption');
+    let secondFigcaption = figcaptions[1];
+    alert(secondFigcaption.innerHTML);
+    
+    // console.log(document.querySelectorAll('figcaption)')[1].innerHTML);
+})
